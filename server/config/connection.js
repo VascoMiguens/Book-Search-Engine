bb0fbc77c9f4bf188f9815610870f73505b1c9c5
VascoMiguens <v_miguens@hotmail.com>
2023-03-06 21:55:28 +0000

    - deploying measures

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/your-database-name",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   }
 );
 
